93edd5f596db88d2c89037a8dae1bcce1cba1402
unknown <Muhammad Hassan Raza>
2024-05-09 00:18:32 +0500

    JavaScript Calculator

---
+++ app.js
@@ -0,0 +1,23 @@
+// JavaScript Calculator
+
+function addValue(numbers) {
+    var input = document.getElementById('input')
+    input.value += numbers
+}
+
+function clean() {
+    var input = document.getElementById('input')
+    input.value = ""
+}
+
+function result() {
+   var input = document.getElementById('input')
+   input.value = eval(input.value)
+}
+// eval value ko calculate karta hai.
+
+// hamny html main function ke name addValue main jo number die 
+// hain wo argument hai.
+
+// or yahan addValue main numbers ka name use kia hai wo parameter hai.
+// user koi bhi number click karega to wo number get hojayega.
